dae8495bd8bc288db832b8b7744d95b5158838b0
Noman Siddiqui <92144508+nomansiddiquiofficial@users.noreply.github.com>
2024-05-14 15:35:10 +0500

    Add project files.

---
+++ Generic methods/Collections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_methods
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string desination { get; set; }
+
+
+
+
+    }
+}
---
+++ Generic methods/Program.cs
@@ -0,0 +1,105 @@
+﻿
+
+
+using Generic_methods;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+class Genric<T>
+{
+     T box;
+    public Genric(T box)
+    {
+        this.box = box;
+
+    }
+
+    public T Box
+    {
+        get
+        {
+            return this.box;
+        }
+
+        set
+        {
+            this.box = value;
+        }
+    }
+
+}
+
+
+
+
+class Progrm
+{
+    //static void Main(string[] args)
+    //{
+    //    Genric<string> genric = new Genric<string>("Noman");
+    //    Genric<int> genric1 = new Genric<int>(1);
+
+    //    C//onsole.WriteLine(genric.Box);
+
+    //    genric1.Box = 2;
+    //    // Console.WriteLine(genric1.Box);
+
+
+    //}
+
+
+    //ListCollection
+    //static void Main(string[] args)
+    //{
+    //    ArrayList myList = new ArrayList();
+    //    myList.Add(20);
+    //    Console.WriteLine(myList);
+    //    Console.WriteLine(myList.Add(myList.Count));
+    //    Console.WriteLine( myList.Contains(20));
+    //    Console.WriteLine(myList.Add("new"));
+    //    foreach (object item in myList)
+    //    {
+    //        Console.Write(item + " ");
+    //    }
+
+
+
+
+    //}
+
+    //Generic List Collection
+   static void Main(string[] args)
+    {
+        
+        //List<string> list = new List<string>();
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Console.Write("Enter names");
+        //    string a = Console.ReadLine();
+        //    list.Add(a.ToString());
+
+        //}
+        //foreach (string s in list)
+        //{
+        //    Console.WriteLine(s," ");
+        //}
+
+        Employee employee = new Employee();
+        employee.name = "Test";
+        employee.age = 1;
+        employee.desination = "CEO";
+
+        List<Employee> Emplist = new List<Employee>();
+        Emplist.Add(employee);
+        foreach (Employee s in Emplist)
+        {
+            Console.WriteLine(s.name + " " );
+        }
+
+
+    }
+
+
+
+
+}
